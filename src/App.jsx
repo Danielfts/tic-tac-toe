977dce502feb633e69505f5634d612587fe14506
Danielfts <d.trivino@uniandes.edu.co>
2024-05-19 18:44:43 -0600

    completed todo

--- src/App.jsx
+++ src/App.jsx
@@ -71,14 +71,14 @@ function Board({ xIsNext, squares, onPlay }) {
 }
 
 export default function Game() {
-  const [history, setHisotry] = useState([Array(9).fill(null)]);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
   const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
-    setHisotry(nextHistory);
+    setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
   }
 
@@ -87,11 +87,15 @@ export default function Game() {
   }
 
   const moves = history.map((squares, move) => {
-    let description;
-    description = move > 0 ? "Go to move #" + move : "Go to game start";
+    let description1;
+    let description2;
+    description1 = move > 0 ? "Go to move #" + move : "Go to game start";
+    description2 = "You are currently at move #"+move;
     return (
       <li key={move}>
-        <button onClick={()=> jumpTo(move)}>{description}</button>
+        
+        {/* <button onClick={()=> jumpTo(move)}>{description}</button> */}
+        {move === currentMove ? (description2) : ( <button onClick={()=> jumpTo(move)}>{description1}</button> )}
       </li>
     )
   });
